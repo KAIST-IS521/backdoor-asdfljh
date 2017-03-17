199f650c5f9cce8718f2e1410006202559a5fc41
Jiho Lee <cjdhlds08@gmail.com>
2017-03-17 15:51:25 +0900

    Modify and test C code files for my interpreter
    interpreter.c와 minivm.c를 수정하고 테스트하기 위하여 인터프리터
    입력으로 사용할 bytecode 파일(test.out)을 생성.
    
    주요 파일 추가 및 수정에 대한 설명.
    
    - interpreter.c: instruction halt, load, store, move, puti, add, sub의
    		구현, instruction 하나 수행될 때마다 register 값 출력
    
    - test.asm: puti, add, sub, halt로만 이루어진 간단한 mini language 코드
    
    - test.out: test.asm 파일을 컴파일한 output 파일

---
+++ compiler/test.asm
@@ -0,0 +1,7 @@
+puti r0, 1
+puti r1, 2
+puti r2, 3
+add r3, r0, r1
+add r4, r1, r2
+sub r5, r2, r0
+halt
--- interpreter/interpreter.c
+++ interpreter/interpreter.c
@@ -8,13 +8,61 @@
 
 #define NUM_REGS   (256)
 #define NUM_FUNCS  (256)
+#define NUM_INSTR  (256)
+
+#define DEBUG 1
 
 // Global variable that indicates if the process is running.
 static bool is_running = true;
 
+uint8_t *ptr_m;
+
 void usageExit() {
     // TODO: show usage
+	printf("USAGE: interpreter [bytecode FILE]\n");
     exit(1);
+}
+
+void halt(struct VMContext* ctx, const uint32_t instr) {
+	is_running = false;
+}
+
+void load(struct VMContext* ctx, const uint32_t instr) {
+	const uint8_t dst = EXTRACT_B1(instr);
+	const uint8_t src = EXTRACT_B2(instr);
+	ctx->r[dst].value = ptr_m[src];
+}
+
+void store(struct VMContext* ctx, const uint32_t instr) {
+	const uint8_t dst = EXTRACT_B1(instr);
+	const uint8_t src = EXTRACT_B2(instr);
+	ptr_m[dst] = EXTRACT_B0(ctx->r[src].value);
+}
+
+void move(struct VMContext* ctx, const uint32_t instr) {
+	const uint8_t dst = EXTRACT_B1(instr);
+	const uint8_t src = EXTRACT_B2(instr);
+	ctx->r[dst].value = ctx->r[src].value;
+}
+
+void puti(struct VMContext* ctx, const uint32_t instr) {
+	const uint8_t reg = EXTRACT_B1(instr);
+	const uint8_t imm = EXTRACT_B2(instr);
+	ctx->r[reg].value = imm;
+}
+
+void add(struct VMContext* ctx, const uint32_t instr) {
+	const uint8_t result = EXTRACT_B1(instr);
+	const uint8_t reg1 = EXTRACT_B2(instr);
+	const uint8_t reg2 = EXTRACT_B3(instr);
+	ctx->r[result].value = ctx->r[reg1].value + ctx->r[reg2].value;
+}
+
+void sub(struct VMContext* ctx, const uint32_t instr) {
+	const uint8_t result = EXTRACT_B1(instr);
+	const uint8_t reg1 = EXTRACT_B2(instr);
+	const uint8_t reg2 = EXTRACT_B3(instr);
+	ctx->r[result].value = ctx->r[reg1].value - ctx->r[reg2].value;
 }
 
 void initFuncs(FunPtr *f, uint32_t cnt) {
@@ -26,6 +74,13 @@ void initFuncs(FunPtr *f, uint32_t cnt) {
     // TODO: initialize function pointers
     // f[0x00] = halt;
     // f[0x10] = load;
+	f[0x00] = halt;
+	f[0x10] = load;
+	f[0x20] = store;
+	f[0x30] = move;
+	f[0x40] = puti;
+	f[0x50] = add;
+	f[0x60] = sub;
 }
 
 void initRegs(Reg *r, uint32_t cnt)
@@ -42,11 +97,16 @@ int main(int argc, char** argv) {
     Reg r[NUM_REGS];
     FunPtr f[NUM_FUNCS];
     FILE* bytecode;
+	uint8_t instr_buffer[NUM_INSTR*4];
     uint32_t* pc;
-
+	int i, j;
+	
+	
     // There should be at least one argument.
     if (argc < 2) usageExit();
-
+	
+	//ptr_m = (uint8_t *)malloc(8192);
+	
     // Initialize registers.
     initRegs(r, NUM_REGS);
     // Initialize interpretation functions.
@@ -60,12 +120,31 @@ int main(int argc, char** argv) {
         perror("fopen");
         return 1;
     }
-
+		
+	i = 0;
+	pc = (uint32_t*)&instr_buffer;
     while (is_running) {
         // TODO: Read 4-byte bytecode, and set the pc accordingly
+		fread(&instr_buffer[i*4], 1, 4, bytecode);
+		printf("Running instruction %d | ", i);
+		for (j = 0; j < 4; ++j)
+		{
+			printf("%.2x ", instr_buffer[i*4+j]);
+		}
+		printf("\n");
+		
         stepVMContext(&vm, &pc);
+#ifdef DEBUG
+		for (j = 0; j < 10; ++j)
+		{
+			printf("r%d: %d\n", j, r[j].value);
+		}
+#endif
+		
+		++i;
     }
-
+	
+	free(ptr_m);
     fclose(bytecode);
 
     // Zero indicates normal termination.
